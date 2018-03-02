d062e81b00d92a7735664e04ef668cca369ca86f
Oleksii Fedorov <waterlink000@gmail.com>
2018-03-02 23:22:59 +0100

    Finish second koan

--- koans/s01_duplicate_code/t02_greet_friends_only.spec.js
+++ koans/s01_duplicate_code/t02_greet_friends_only.spec.js
@@ -15,4 +15,49 @@ describe('02 - greet friends only', () => {
     it('does not greet Sam because she is not a friend', () => {
         expect(greet('Sam')).to.eq('');
     });
+
+    describe('use name parameter instead of hard-coding', () => {
+        it('does not hard-code greeting to Amy or Bob', () => {
+            expect(greet.toString()).to.not.include('Hello Amy');
+            expect(greet.toString()).to.not.include('Hello Bob');
+        });
+    });
+
+    describe('extract method for duplicated if body', () => {
+        it('uses greetByName method instead of duplicating', () => {
+            expect(greet.toString()).to.not.include('`Hello ${name}!`');
+            expect(greet.toString()).to.include('return code.greetByName(name)');
+        });
+    });
+
+    describe('consolidate conditional expression', () => {
+        it('has only single if statement', () => {
+            expect(greet.toString().match(/if/g)).to.have.length(1);
+        });
+    });
+
+    describe('extract condition as a method', () => {
+        it('has method isFriend', () => {
+            expect(code['isFriend']('Amy')).to.eq(true);
+            expect(code['isFriend']('Bob')).to.eq(true);
+            expect(code['isFriend']('Sam')).to.eq(false);
+            expect(code['isFriend']('Boris')).to.eq(false);
+        });
+
+        it('does not have a complex conditional', () => {
+            expect(greet.toString()).to.not.include('||');
+            expect(greet.toString()).to.not.include('==');
+        });
+    });
+
+    describe('use appropriate data structure', () => {
+        it('has field friends', () => {
+            expect(code['friends']).to.deep.eq(['Amy', 'Bob']);
+        });
+
+        it('has simple conditional in isFriend method', () => {
+            expect(code['isFriend'].toString()).to.not.include('||');
+            expect(code['isFriend'].toString()).to.not.include('==');
+        });
+    });
 });
