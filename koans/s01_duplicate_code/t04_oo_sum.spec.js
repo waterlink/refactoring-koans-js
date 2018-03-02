b6f6d7f7058323da6f8d0f689942dfc2a002ebaa
Oleksii Fedorov <waterlink000@gmail.com>
2018-03-03 00:55:09 +0100

    Finish 4th koan

--- koans/s01_duplicate_code/t04_oo_sum.spec.js
+++ koans/s01_duplicate_code/t04_oo_sum.spec.js
@@ -1,4 +1,5 @@
 const {expect} = require('chai');
+const {ownerOf} = require('../../util');
 
 const code = require('./t04_oo_sum');
 const {Summer, Producter} = code;
@@ -29,5 +30,62 @@ describe('04 - object-oriented sum', () => {
 
     it('prod of 1 to 9 is 362.880', () => {
         expect(producter.aggregate(9)).to.eq(362880);
+    });
+
+    xdescribe('extract method initial value for result as a method', () => {
+        it('summer.aggregate uses initial value method', () => {
+            expect(summer.aggregate.toString()).to.not.include('result = 0');
+            expect(summer.aggregate.toString()).to.include('result = this.getInitialValue()');
+        });
+
+        it('producter.aggregate uses initial value method', () => {
+            expect(producter.aggregate.toString()).to.not.include('result = 1');
+            expect(producter.aggregate.toString()).to.include('result = this.getInitialValue()');
+        });
+    });
+
+    xdescribe('replace initial loop index value using initial value method', () => {
+        it('summer.aggregate does not hard-code initial loop index', () => {
+            expect(summer.aggregate.toString()).to.not.include('i = 0');
+            expect(summer.aggregate.toString()).to.not.include('i = 1');
+        });
+
+        it('producter.aggregate does not hard-code initial loop index', () => {
+            expect(producter.aggregate.toString()).to.not.include('i = 1');
+            expect(producter.aggregate.toString()).to.not.include('i = 2');
+        });
+    });
+
+    xdescribe('extract aggregate function as a method', () => {
+        it('summer.aggregate does not use +', () => {
+            expect(summer.aggregate.toString()
+                .replace('++', '')
+                .replace('this.getInitialValue() + 1', ''))
+                .to.not.include('+');
+
+            expect(summer.aggregate.toString()).to.include('result = this.aggregateFunction(');
+        });
+
+        it('producter.aggregate does not use *', () => {
+            expect(producter.aggregate.toString()).to.not.include('*');
+            expect(producter.aggregate.toString()).to.include('result = this.aggregateFunction(');
+        });
+
+        it('summer.aggregate and producter.aggregate are identical', () => {
+            expect(summer.aggregate.toString())
+                .to.eq(producter.aggregate.toString());
+        });
+    });
+
+    xdescribe('extract aggregator base class', () => {
+        it('summer.aggregate is implemented on the base class', () => {
+            expect(ownerOf(summer, 'aggregate')).to.not.eq('Summer');
+            expect(ownerOf(summer, 'aggregate')).to.not.eq('Producter');
+        });
+
+        it('producter.aggregate is implemented on the base class', () => {
+            expect(ownerOf(producter, 'aggregate')).to.not.eq('Summer');
+            expect(ownerOf(producter, 'aggregate')).to.not.eq('Producter');
+        });
     });
 });
--- util/index.js
+++ util/index.js
@@ -4,3 +4,13 @@ module.exports.t03_stillPreparing = () => {
     return !code.sum.toString().includes('code.aggregate(') &&
         !code.prod.toString().includes('code.aggregate(');
 };
+
+module.exports.ownerOf = (instance, property) => {
+    const hasOwnProperty = Object.prototype.hasOwnProperty;
+
+    while (instance && !hasOwnProperty.call(instance, property)) {
+        instance = Object.getPrototypeOf(instance);
+    }
+
+    return instance && instance.constructor.name;
+};
